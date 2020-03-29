2e7d35c939a168e909e368766d54bc906fd5212a
ceesargtz <ceesar_gtz@hotmail.com>
2020-03-29 07:57:35 -0600

    Place holder
    Se cambia el place holder de acuerdo al tamaño del dispositivo.

--- src/app/components/datos/datos.component.ts
+++ src/app/components/datos/datos.component.ts
@@ -6,8 +6,24 @@ declare var $:any
   styles: []
 })
 export class DatosComponent implements OnInit {
-
+nombres:string
+apellido_paterno:string
+apellido_materno:string
+correo:string
   constructor() {
+    if (screen.width>1000){
+      this.nombres="ESCRIBE TU NOMBRE"
+      this.apellido_paterno="ESCRIBE TU APELLIDO"
+       this.apellido_materno="ESCRIBE TU APELLIDO"
+      this.correo="CORREO@EMAIL.COM.MX"
+      }
+        if(screen.width<1000){
+          this.nombres="EJ. JUAN"
+          this.apellido_paterno="EJ. LÓPEZ"
+           this.apellido_materno="EJ. TOVAR"
+          this.correo="TU@EMAIL.COM"
+        }
+
 
     }
 
